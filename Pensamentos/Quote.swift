b9791bb4766a0c5049ca083519546f085bab0752
Usuário Convidado <Guest@pa06micro21.local>
2018-03-14 23:03:52 -0300

    APP 90 por cento pronto

---
+++ Pensamentos/Quote.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+struct Quote: Codable { //Encodable, Decodable
+    
+    let quote: String
+    let author: String
+    let image: String
+    
+    
+    var quoteFormatted: String {
+        return "〝" + quote + "〞"
+    }
+    
+    var authorFormatted: String {
+        return "- " + author + " -"
+    }
+ 
+}
